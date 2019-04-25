83d736b4fe199e1ceac2afef3122f8434ad42429
Tyler Kelly <tkelly10@my.wctc.edu>
2019-04-24 19:21:07 -0500

    Strategy Pattern

---
+++ src/com/company/Student.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Student  {
+
+    private String name;
+    private int age;
+
+    public Address address;
+    
+    public Student(String streetAddress, int zipCode, String state, String phone, String name, int age) {
+        this.name = name;
+        this.age = age;
+        address = new Address(streetAddress, zipCode,  state, phone);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public String getState()
+    {
+        return address.getState();
+    }
+}
